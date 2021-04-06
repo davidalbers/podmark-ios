60cefe0bf8f77d793b445ee15c74f87cc5f23be7
David Albers <dga148@utulsa.edu>
2021-04-05 20:28:39 -0500

    Folders part 2: add and delete

--- podmark/MainViewController.swift
+++ podmark/MainViewController.swift
@@ -13,9 +13,27 @@ class MainViewController: UITableViewController {
         title = "Folders"
         presenter.$folders.sink { [weak self] folders in
             self?.folders = folders
+            self?.tableView?.reloadData()
         }.store(in: &cancellables)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
     }
 
+    @objc func addTapped() {
+        let alert = UIAlertController(title: "Add a folder", message: nil, preferredStyle: .alert)
+        alert.addTextField {
+            $0.placeholder = "Folder name"
+        }
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let submitAction = UIAlertAction(title: "Add", style: .default) { [unowned alert, weak self] _ in
+            self?.presenter.addFolder(folderName: alert.textFields![0].text)
+        }
+
+        alert.addAction(cancelAction)
+        alert.addAction(submitAction)
+
+        present(alert, animated: true)
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return folders.count
@@ -32,5 +50,12 @@ class MainViewController: UITableViewController {
         let savedListView = SavedListView(folderName: folders[indexPath.row])
         let savedListVC = UIHostingController(rootView: savedListView)
         navigationController!.pushViewController(savedListVC, animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let folderName = folders.remove(at: indexPath.row)
+            presenter.deleteFolder(folderName)
+        }
     }
 }
