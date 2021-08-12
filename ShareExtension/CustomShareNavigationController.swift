e0180fe763132d660fe9c2a6e915dfb8d6757e0b
David Albers <davidgeorgea@gmail.com>
2021-08-11 19:27:05 -0500

    Parse all shared  rather than the first

--- ShareExtension/CustomShareNavigationController.swift
+++ ShareExtension/CustomShareNavigationController.swift
@@ -32,19 +32,21 @@ class CustomShareNavigationController: UIViewController {
     override func beginRequest(with context: NSExtensionContext) {
         self.context = context
         if let item = context.inputItems.first as? NSExtensionItem,
-           let attachments = item.attachments,
-           let itemProvider = attachments.first {
-            if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
-                itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil) { [self] (url, error) in
-                    if let url = (url as? URL) {
-                        shareView?.shareViewModel.setURL(url.absoluteString)
+           let attachments = item.attachments {
+            attachments.forEach { itemProvider in
+                if itemProvider.hasItemConformingToTypeIdentifier("public.url") {
+                    itemProvider.loadItem(forTypeIdentifier: "public.url", options: nil) { [self] (url, error) in
+                        if let url = (url as? URL) {
+                            shareView?.shareViewModel.setURL(url.absoluteString)
+                        }
                     }
                 }
-            }
-            if itemProvider.hasItemConformingToTypeIdentifier("public.plain-text") {
-                itemProvider.loadItem(forTypeIdentifier: "public.plain-text", options: nil) { [self] (sharedText, error) in
-                    if let sharedText = (sharedText as? String) {
-                        shareView?.shareViewModel.setURL(sharedText)
+                if itemProvider.hasItemConformingToTypeIdentifier("public.plain-text") {
+                    itemProvider.loadItem(forTypeIdentifier: "public.plain-text", options: nil) { [self] (sharedText, error) in
+                        if let sharedText = (sharedText as? String),
+                           let sharedTextURL = URL(string: sharedText){
+                            shareView?.shareViewModel.setURL(sharedTextURL.absoluteString)
+                        }
                     }
                 }
             }
